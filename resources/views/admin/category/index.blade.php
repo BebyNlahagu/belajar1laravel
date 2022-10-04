d34899d9892076e64e822348e82d3ccbdb89257c
bebynlahagu <bebynlahagu20@gmail.com>
2022-10-04 12:30:01 +0700

    first commit

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-12 grid-margin">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Category</h3>
+                        <a href="{{url('admin/category/create')}}" class="btn btn-primary btn-sm float-end">Tambah +</a>
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+
+
